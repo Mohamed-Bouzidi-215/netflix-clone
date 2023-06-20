8b559d55f4291a5ad57ab3d1a0fa1d97643cf458
Mohamed-Bouzidi-215 <mohamed.bouzidi.215@gmail.com>
2023-06-20 21:53:35 +0200

    Create functions to create user and sign in

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCJLDDlmPBH4KLwznJv7TZOHlipNVRNKSI",
     authDomain: "netflix-clone-5d9bc.firebaseapp.com",
@@ -6,4 +11,10 @@ const firebaseConfig = {
     messagingSenderId: "794399866620",
     appId: "1:794399866620:web:f6e8f9ed1fec592feda179"
   };
-  
+
+const firebaseapp = firebase.initializeApp(firebaseConfig);
+const db = firebaseapp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
