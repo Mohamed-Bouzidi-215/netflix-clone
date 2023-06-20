69a03c4827e54554112483d13b19d1b00602c9b1
Mohamed-Bouzidi-215 <mohamed.bouzidi.215@gmail.com>
2023-06-20 22:04:58 +0200

    UseEffect for securize authentification

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { auth } from './firebase';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 
 function App() {
-const user = null;
+
+  const user = null;
+
+  useEffect(() => {
+    const unsuscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // logged in
+        console.log(userAuth);
+      } else {
+        // logged out
+      }
+      })
+
+    return unsuscribe;
+    }, []);
 
   return (
     <div className="app">
