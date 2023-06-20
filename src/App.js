5640ed45fa29308f82671e1fde0f0cc2025b18bd
Mohamed-Bouzidi-215 <mohamed.bouzidi.215@gmail.com>
2023-06-20 15:29:28 +0200

    Install React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,10 @@
 // == Import
 import React from 'react';
 import './HomeScreen.css';
-import Nav from './Nav';
-import Banner from './Banner';
-import Row from './Row';
-import requests from './Requests';
+import Nav from '../Nav';
+import Banner from '../Banner';
+import Row from '../Row';
+import requests from '../Requests';
 
 // == Composant
 function HomeScreen() {
