c09e1e5419b111d781e4b4803a6dd417be51dc8c
Mohamed-Bouzidi-215 <mohamed.bouzidi.215@gmail.com>
2023-06-20 16:51:55 +0200

    Build LoginScreen component - Start SignupScreen component

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignupScreen.css';
+
+function SignupScreen() {
+  return (
+    <div className='signupScreen'>
+        SignupScreen
+    </div>
+  )
+}
+
+export default SignupScreen
