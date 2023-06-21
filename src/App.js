04f9c3d09bbd9ff1787eb98c0d482045780ef736
Mohamed-Bouzidi-215 <mohamed.bouzidi.215@gmail.com>
2023-06-21 13:49:54 +0200

    Login & Logout functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { auth } from './firebase';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
-
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsuscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
         // logged in
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+        }));
       } else {
         // logged out
+        dispatch(logout);
       }
-      })
+    });
 
     return unsuscribe;
-    }, []);
+  }, );
 
   return (
     <div className="app">
